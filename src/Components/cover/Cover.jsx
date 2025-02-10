c8d56ac39fa63c8a60a4bbe8bfb3c48af75c84f1
EzequielEche <eche_@hotmail.com>
2025-02-10 15:42:01 -0300

    script añadido

--- src/Components/cover/Cover.jsx
+++ src/Components/cover/Cover.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import './Cover.css';
-import coverVideo from '../../Media/coverVideo.mp4';
+import React from "react";
+import "./Cover.css";
+import coverVideo from "../../Media/coverVideo.mp4";
 import { motion } from "framer-motion";
 
 const Cover = () => {
   return (
-    <div className='cover-container'>
-<video className='video' src={coverVideo} autoPlay loop muted/>
-<motion.div initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration:2}}  viewport={{once:false}}>
-  <div className='text'>
-      <h1>
-    Ezequiel Sanchez Pais
-</h1>
-<p> Desarrollador Web</p>
-  </div>
-
-</motion.div>
-
+    <div className="cover-container">
+      <video className="video" src={coverVideo} autoPlay loop muted />
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 2 }}
+        viewport={{ once: false }}
+      >
+        <div className="text">
+          <h1>Ezequiel Sánchez Pais</h1>
+          <p> Desarrollador Web</p>
+        </div>
+      </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default Cover
+export default Cover;
