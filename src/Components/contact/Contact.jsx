f9314ff2815718facfd21fbf31b7d3bef71e198b
EzequielEche <eche_@hotmail.com>
2025-02-14 15:01:02 -0300

    corecciones responsive

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./Contact.css";
 
-
-const Contact = ({on}) => {
+const Contact = ({ on }) => {
   return (
     <div className="contact-container" data-on={on}>
       <h2 className="h2-c">CONTACTO</h2>
-      <div className="div-contact" >
+      <div className="div-contact">
         <a href="https://github.com/EcheMacaco" alt="github" data-on={on}>
           Github
         </a>
-        <a href="https://linkedin.com/in/ezequiel-sanchez-pais" data-on={on}>Linkedin</a>
+        <a href="https://linkedin.com/in/ezequiel-sanchez-pais" data-on={on}>
+          Linkedin
+        </a>
         <p data-on={on}> Telefono: (221) 3143643 </p>
         <p data-on={on}> Mail: eche_@hotmail.com</p>
       </div>
